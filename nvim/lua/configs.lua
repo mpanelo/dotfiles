181b1ed1414c4a64ffe052576c6663042dd838b5
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2023-03-11 23:25:33 -0800

    Set vim options for indentation

--- nvim/lua/configs.lua
+++ nvim/lua/configs.lua
@@ -12,3 +12,27 @@ vim.opt.number = true
 
 vim.cmd([[colorscheme sonokai]])
 -- vim.cmd([[colorscheme dracula]])
+
+-- In Insert mode: Use the appropriate number of spaces to insert a
+-- <Tab>.  Spaces are used in indents with the '>' and '<' commands and
+-- when 'autoindent' is on.
+vim.opt.expandtab = true
+
+-- Copy indent from current line when starting a new line (typing <CR>
+-- in Insert mode or when using the "o" or "O" command).
+vim.opt.autoindent = true
+
+-- Do smart autoindenting when starting a new line.
+vim.opt.smartindent = true
+
+-- TODO figure out how to change indentation based on filetype
+
+-- Number of spaces that a <Tab> in the file counts for.
+vim.opt.tabstop = 4
+
+-- Number of spaces that a <Tab> counts for while performing editing
+-- operations, like inserting a <Tab> or using <BS>.
+vim.opt.softtabstop = 4
+
+-- Number of spaces to use for each step of (auto)indent.
+vim.opt.shiftwidth = 4
