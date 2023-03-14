d49ac129c0fa4c3fb5fcecbe996a0a39262e1516
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2023-03-13 23:23:11 -0700

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,25 @@
 # Homebrew
 eval (/opt/homebrew/bin/brew shellenv)
 
-alias vim="nvim"
+# Zoxide
+zoxide init fish | source
+
+# Starship prompt
+starship init fish | source
+
 
 # Rust/Cargo
 fish_add_path "$HOME/.cargo/bin/"
 
 # Go
 fish_add_path /usr/local/go/bin
+fish_add_path "$HOME/go/bin"
 
-zoxide init fish | source
+alias v="nvim"
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
 
-starship init fish | source
+set -Ux fish_greeting
 
 # Load configurations which should not be stored in a version control system (i.e. API keys, sensitive data, persional info, etc.)
 set LOCALCFG (dirname (status --current-filename))/config.local.fish
