688a63dc53548513bd6b78bdeed39c5253107a4a
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2023-03-26 23:14:38 -0700

    Add gitsigns plugin for neovim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -112,4 +112,12 @@ return packer.startup(function(use)
 			require("alpha").setup(require("alpha.themes.startify").config)
 		end,
 	})
+
+	use({
+		"lewis6991/gitsigns.nvim",
+		-- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
 end)
