0745bf3bf1204fcc2067803b9bbfaacb702144c8
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2024-07-14 17:12:25 -0700

    Use new colorscheme for neovim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,17 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    -- the colorscheme should be available when starting Neovim
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        opts = {
+            style = "night",
+        },
+        config = function(_, opts)
+            require("tokyonight").setup(opts)
+
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
 }
