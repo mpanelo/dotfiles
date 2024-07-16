c2986ed157e515321a3ad901402d55ab06b5ea2c
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2024-07-15 19:45:49 -0700

    Setup gopls for neovim and update Telescope keybindings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,56 +1,91 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "mason.nvim",
-            { "williamboman/mason-lspconfig.nvim", config = function() end },
-        },
-        config = function()
-            local lspconfig = require('lspconfig')
-            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"mason.nvim",
+			{ "williamboman/mason-lspconfig.nvim", config = function() end },
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            lspconfig.gopls.setup {
-                capabilities = lsp_capabilities,
-                settings = {
-                    gopls = {
-                        analyses = {
-                            unusedparams = true,
-                            useany = true,
-                        },
-                        staticcheck = true,
-                    },
-                },
-            }
+			local on_attach = function(_, bufnr)
+				local function map(lhs, rhs, custom_opts)
+					local opts = { noremap = true, silent = true, buffer = bufnr }
+					for k, v in pairs(custom_opts) do
+						opts[k] = v
+					end
+					vim.keymap.set("n", lhs, rhs, opts)
+				end
 
-            lspconfig.tsserver.setup {
-            }
+				map("gd", function()
+					require("telescope.builtin").lsp_definitions({ reuse_win = true })
+				end, { desc = "Goto Definition" })
+				map("gr", "<cmd>Telescope lsp_references<cr>", { desc = "References", nowait = true })
+				map("gI", function()
+					require("telescope.builtin").lsp_implementations({ reuse_win = true })
+				end, { desc = "Goto Implementation" })
+				map("gy", function()
+					require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
+				end, { desc = "Goto T[y]pe Definition" })
+			end
 
-            lspconfig.lua_ls.setup {
-                capabilities = lsp_capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global
-                            globals = {
-                                'vim',
-                            }
-                        }
-                    },
-                },
-            }
-        end
-    },
-    {
-        "williamboman/mason.nvim",
-        cmd = "Mason",
-        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-        build = ":MasonUpdate",
-        opts = {
-            ensure_installed = {
-                "stylua",
-                "shfmt",
-            },
-        },
-    },
+			lspconfig.gopls.setup({
+				on_attach = on_attach,
+				capabilities = lsp_capabilities,
+				settings = {
+					gopls = {
+						gofumpt = true,
+						hints = {
+							functionTypeParameters = true,
+							parameterNames = true,
+							constantValues = true,
+							rangeVariableTypes = true,
+						},
+						analyses = {
+							unusedparams = true,
+							useany = true,
+						},
+						staticcheck = true,
+						directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+					},
+				},
+			})
+
+			lspconfig.tsserver.setup({
+				on_attach = on_attach,
+				capabilities = lsp_capabilities,
+			})
+
+			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
+				capabilities = lsp_capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = {
+								"vim",
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+		build = ":MasonUpdate",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shfmt",
+				"goimports",
+				"gofumpt",
+			},
+		},
+	},
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,22 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 'nvim-lua/plenary.nvim' }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-lua/plenary.nvim" },
+	},
+	keys = {
+		{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+		-- Find
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
+		{ "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+		{ "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find File (git-files)" },
+		{ "<leader>fr", "<cmd>Telescope oldfiles cwd_only=true<cr>", desc = "Recent" },
+		-- Git
+		{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
+		-- Search
+		{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+		{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Search Help Pages" },
+		{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Search Man Pages" },
+	},
 }
