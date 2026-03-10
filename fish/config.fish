d77ba2e7198c174391d0bb58d40a63b32eb6bdda
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2026-03-09 17:34:13 -0700

    Add missing fishshell folders

--- fish/config.fish
+++ fish/config.fish
@@ -6,17 +6,8 @@ fzf --fish | source
 # Zoxide
 zoxide init fish | source
 
-# Starship prompt
-starship init fish | source
-
-# Rust/Cargo
 fish_add_path "$HOME/.cargo/bin/", "$HOME/go/bin"
-
 #fish_add_path "/Applications/Alacritty.app/Contents/MacOS"
-
-# Go
-fish_add_path /usr/local/go/bin
-fish_add_path "$HOME/go/bin"
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
@@ -25,3 +16,6 @@ set -gx fish_greeting
 abbr lg "lazygit"
 #abbr tn "tmux new -s"
 #abbr fs "tmux ls -F '#{session_name}' | fzf-tmux -p | xargs tmux switch -t"
+
+# Starship prompt
+starship init fish | source
