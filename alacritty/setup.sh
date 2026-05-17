59a78a33cea313a5b566c27bc23f21779733cac4
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2026-05-17 01:21:39 -0700

    Update theme for alacritty

---
+++ alacritty/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Alacritty theme
+# README https://github.com/catppuccin/alacritty
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
